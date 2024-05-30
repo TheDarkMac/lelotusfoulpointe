59f8ddbdd4763754d4a19ede202dce3d7a69b0b3
ando <hei.ando.14@gmail.com>
2024-05-30 22:03:25 +0300

    fix: fixed pink color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        lotus: '#f13fff'
+        lotus: {
+          green1:   '#74bc44',
+          pink1 :   '#f33cfc',
+          violet1:  '#945bfc',
+          green2:   '#a6d284',
+          pink2:    '#f46cfc',
+          violet2:  '#d450ff'
+        }
       }
     },
   },
