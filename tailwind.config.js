773d6790ed5030c965f61a49c401f03fa06d6bac
ando <hei.ando.14@gmail.com>
2024-05-31 23:47:20 +0300

    feat: make background darker

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,30 @@ module.exports = {
           pink2:    '#f46cfc',
           violet2:  '#d450ff'
         }
+      },
+      backgroundImage : {
+        BUNGALOW_01_1_dark : 'linear-gradient(to bottom right, rgba(0,0,0,0.6) , rgba(0,0,0,0.6)) , url("../images/BUNGALOW_01_1.jpg")',
+        BUNGALOW_01_1 : ' url("../images/BUNGALOW_01_1.jpg")',
+        BUNGALOW_01_2_dark : 'linear-gradient(to bottom right, rgba(0,0,0,0.6) , rgba(0,0,0,0.6)) , url("../images/BUNGALOW_01_2.jpg")',
+        BUNGALOW_01_2 : ' url("../images/BUNGALOW_01_2.jpg")',
+        BUNGALOW_01_dark : 'linear-gradient(to bottom right, rgba(0,0,0,0.6) , rgba(0,0,0,0.6)) , url("../images/BUNGALOW_01.jpg")',
+        BUNGALOW_01 : ' url("../images/BUNGALOW_01.jpg")',
+        BUNGALOW_01_01_dark : 'linear-gradient(to bottom right, rgba(0,0,0,0.6) , rgba(0,0,0,0.6)) , url("../images/BUNGALOW_01_01.jpg")',
+        BUNGALOW_01_01 : ' url("../images/BUNGALOW_01_01.jpg")',
+        BUNGALOW_03_dark : 'linear-gradient(to bottom right, rgba(0,0,0,0.6) , rgba(0,0,0,0.6)) , url("../images/BUNGALOW_03.jpg")',
+        BUNGALOW_03 : ' url("../images/BUNGALOW_03.jpg")',
+        BUNGALOW_04_dark : 'linear-gradient(to bottom right, rgba(0,0,0,0.6) , rgba(0,0,0,0.6)) , url("../images/BUNGALOW_03.jpg")',
+        BUNGALOW_04 : ' url("../images/BUNGALOW_04.jpg")',
+        BUNGALOW_NIGHT_01_dark : 'linear-gradient(to bottom right, rgba(0,0,0,0.6) , rgba(0,0,0,0.6)) , url("../images/BUNGALOW_NIGHT_01.jpg")',
+        BUNGALOW_NIGHT_01 : ' url("../images/BUNGALOW_NIGHT_01.jpg")',
+        cover_image_dark : 'linear-gradient(to bottom right, rgba(0,0,0,0.6) , rgba(0,0,0,0.6)) , url("../images/cover_image.jpg")',
+        cover_image : ' url("../images/cover_image.jpg")',
+        EXTERIEUR_01_dark : 'linear-gradient(to bottom right, rgba(0,0,0,0.6) , rgba(0,0,0,0.6)) , url("../images/EXTERIEUR_01.jpg")',
+        EXTERIEUR_01 : ' url("../images/EXTERIEUR_01.jpg")',
+        EXTERIEUR_02_dark : 'linear-gradient(to bottom right, rgba(0,0,0,0.6) , rgba(0,0,0,0.6)) , url("../images/EXTERIEUR_02.jpg")',
+        EXTERIEUR_02 : ' url("../images/EXTERIEUR_02.jpg")',
+
+        
       }
     },
   },
